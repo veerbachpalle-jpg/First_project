6b34762f06a0a62aa63bfcd573790c6bc600b083
veerbachpalle-jpg <veerbachpalle@gmail.com>
2026-06-23 17:26:13 +0530

    added Subcription model

---
+++ src/models/subcription.model.js
@@ -0,0 +1,16 @@
+import mongoose,{Schema} from "mongoose";
+import { User } from "./user.model";
+
+const subcriptionSchema = new Schema({
+  subscriber:{
+    type: Schema.Types.ObjectId,
+    ref:User
+  },
+  channel:{
+    type:Schema.Types.ObjectId,
+    ref:User
+  }
+
+})
+
+export const Subcription = mongoose.model("Subcription",subcriptionSchema)
