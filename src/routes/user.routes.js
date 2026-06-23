d3cd1431fe5fc9573b7e7009dd2f88e571fe72a3
veerbachpalle-jpg <veerbachpalle@gmail.com>
2026-06-24 02:19:55 +0530

    connect frontend

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -27,7 +27,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-User").get(verifyJWT,getCurrentUser)
 router.route("/update-account"),patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverimage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverimage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 
